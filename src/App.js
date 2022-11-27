84804e18ba215643eebedbe9d49f3ac95dd63df6
송창욱 <changwook@MacBookPro.local>
2022-11-28 02:34:53 +0900

    08-09.sort로 데이터 정렬

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import ReviewList from "./components/ReviewList";
 import items from "./mock.json";
 
 function App() {
+    const [order, setOrder] = useState("createdAt");
+    const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+    const handleNewestClick = () => setOrder("createdAt");
+
+    const handleBestClick = () => setOrder("rating");
+
     return (
         <div>
-            <ReviewList items={items} />
+            <div>
+                <button onClick={handleNewestClick}>최신순</button>
+                <button onClick={handleBestClick}>별점순</button>
+            </div>
+            <ReviewList items={sortedItems} />
         </div>
     );
 }
