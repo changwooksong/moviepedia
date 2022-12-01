97c54b304ef4b7c95940230a672ef39bc254263f
changwook <scw9327@naver.com>
2022-12-01 20:06:31 +0900

    4 - 01. 글 작성하기

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,15 +1,20 @@
 import { useState } from "react";
+import { createReview } from "../api";
 import FileInput from "./FileInput";
 import RatingInput from "./RatingInput";
 import "./ReviewForm.css";
 
+const INITIAL_VALUES = {
+    title: "",
+    rating: 0,
+    content: "",
+    imgFile: null,
+};
+
 function ReviewForm() {
-    const [values, setValues] = useState({
-        title: "",
-        rating: 0,
-        content: "",
-        imgFile: null,
-    });
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [submittingError, setSubmittingError] = useState(null);
+    const [values, setValues] = useState(INITIAL_VALUES);
 
     const handleChange = (name, value) => {
         setValues((prevValues) => ({
@@ -23,9 +28,26 @@ function ReviewForm() {
         handleChange(name, value);
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(values);
+        const formData = new FormData();
+        formData.append("title", values.title);
+        formData.append("rating", values.rating);
+        formData.append("content", values.content);
+        formData.append("imgFile", values.imgFile);
+
+        try {
+            setSubmittingError(null);
+            setIsSubmitting(true);
+            await createReview(formData);
+        } catch (error) {
+            setSubmittingError(error);
+            return;
+        } finally {
+            setIsSubmitting(false);
+        }
+
+        setValues(INITIAL_VALUES);
     };
 
     return (
@@ -50,7 +72,10 @@ function ReviewForm() {
                 value={values.content}
                 onChange={handleInputChange}
             />
-            <button type="submit">확인</button>
+            <button type="submit" disabled={isSubmitting}>
+                확인
+            </button>
+            {submittingError?.message && <div>{submittingError.messge}</div>}
         </form>
     );
 }
